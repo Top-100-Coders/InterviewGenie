5d00e1d5691ba235647fa67ac238583579cbbcd4
Amar Diyab <diyabamar3043@gmail.com>
2023-11-17 10:24:49 +0530

    init commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,139 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import logo from "../public/ai-logo.svg";
+import TypeIt from "typeit-react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
+  // const stringSplitter = (string) => {
+  //   const splitter = new GraphemeSplitter();
+  //   return splitter.splitGraphemes(string);
+  // };
+  const [value, setValue] = useState("");
+  const [message, setMessage] = useState(null);
+  const [prevChats, setPrevChats] = useState([]);
+  const [currentTitle, setCurrentTitle] = useState([]);
+  
+  const inputRef = useRef(null);
+  const containerRef = useRef(null);
+
+
+  useEffect(() => {
+    if (!currentTitle && value && message) {
+      setCurrentTitle(value);
+    }
+
+    if (currentTitle && value && message) {
+      setPrevChats((prevChats) => [
+        ...prevChats,
+        {
+          title: currentTitle,
+          role: "user",
+          content: value,
+        },
+        {
+          title: currentTitle,
+          role: message.role,
+          content: message.content,
+        },
+      ]);
+    }
+  }, [message, currentTitle, value]);
+
+  const token = "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";
+
+  const chat = async () => {
+    const inputValue = inputRef.current.value;
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: inputValue,
+      }),
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const resp = await fetch("http://localhost:5500/completions", options);
+      const data = await resp.json();
+      setMessage(data.choices[0].message);
+      setValue(inputValue);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+
+    inputRef.current.value = "";
+  };
+
+  const currentChat = prevChats.filter(
+    (prevChat) => prevChat.title === currentTitle
+  );
+  console.log(currentChat.length, "cht");
+
+  useEffect(() => {
+    const container = containerRef.current;
+    if (container) {
+      container.scrollTop = container.scrollHeight;
+    }
+  }, [message]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="h-auto flex flex-col items-center" ref={containerRef}>
+      <div className="flex flex-col space-y-5 justify-center items-center mt-[15rem]">
+        <Image src={logo} alt="AI LOGO" width={100} />
+        {currentChat.length === 0 && (
+          <>
+            <input
+              type="text"
+              placeholder="JOB ROLE"
+              className="px-3 py-1 rounded-lg"
+              // value={value}
+              ref={inputRef}
             />
-          </a>
+            <button
+              type="submit"
+              className="rounded-[10px] text-white bg-[#375ac1] hover:bg-[#3766f28e] px-4 py-2"
+              onClick={chat}
+            >
+              START INTERVIEW
+            </button>
+          </>
+        )}
+      </div>
+
+      <div className="mb-[5rem] w-[90%] flex flex-col justify-center items-center">
+        {currentChat.map((chatMessage, index) => (
+          <>
+            <span className="text-white ml-[5rem] self-start mt-10 ">
+              {chatMessage.role}
+            </span>
+            <div className="w-[90%] text-white bg-[#375ac1] h-auto p-10 rounded-lg  ">
+              <TypeIt options={{ speed: 50, cursor: false }}>
+                {chatMessage.content}
+              </TypeIt>
+            </div>
+          </>
+        ))}
+      </div>
+
+      {currentChat.length > 0 && (
+        <div className="flex justify-center fixed bottom-10 w-[95%] space-x-7">
+          <input
+            type="text"
+            ref={inputRef}
+            className="w-[80%] self-start rounded-lg px-2 py-3"
+            placeholder="Type your answer !"
+          />
+          <button
+            className="rounded-[10px] text-gray bg-white hover:bg-white px-4 py-2"
+            onClick={chat}
+          >
+            &gt;
+          </button>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      )}
     </main>
-  )
+  );
 }
---
+++ server/server.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import cors from 'cors';
+import OpenAi from 'openai'
+const port = 5500;
+
+const openai = new OpenAi({apiKey:"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"})
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('API running'));
+app.use(cors({
+    origin: '*', // Replace with your frontend's domain
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify allowed HTTP methods
+    credentials: true, // Allow sending cookies and credentials with requests, // Respond with 204 No Content for OPTIONS requests
+    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization', // Specify allowed headers
+}))
+const token = "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";
+app.post('/completions', async(req, res) => {
+    const options={
+        method:"POST",
+        
+        headers:{
+            "Authorization":`Bearer ${token}`,
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify({
+            model:"gpt-3.5-turbo",
+            messages:[{role:"system",content:"I want you to act as an interviewer. I will be the candidate and you will ask me theinterview questions for the position reactjs developer. I want you to only reply as the interviewer.Do not write all the conservation at once. I want you to only do the interview with me. Ask me the questions and wait for my answers. Do not write explanations. Ask me the questions one by one like an interviewer does and wait for my answers. My first sentence is Hi"},{role:"user",content: req.body.message,  }],
+            max_tokens:100,
+        })
+    }
+ try {
+    const resp = await fetch("https://api.openai.com/v1/chat/completions", options)
+    const data=await resp.json()
+    const mp3 = await openai.audio.speech.create({
+        model: "tts-1",
+        voice: "alloy",
+        input: "Today is a wonderful day to build something people love!",
+    });
+    res.send(data)
+ } catch (error) {
+    console.log(error);
+ }
+    });
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
