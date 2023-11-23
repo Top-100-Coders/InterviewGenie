aeb1aef2addf095864b62e092007bdd61f4f4590
Amar Diyab <diyabamar3043@gmail.com>
2023-11-23 11:13:57 +0530

    Protected .env and changed API_KEY. Minimal changes in prompt

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.post('/questions', async(req, res) => {
         },
         body: JSON.stringify({
             model:"gpt-3.5-turbo",
-            messages:[{role:"system",content:`I want you to act as an interviewer. I will be the candidate and you will ask me the interview questions for the position of ReactJS Developer. I want you to only reply as the interviewer.Do not write all the conservation at once. I want you to only do the interview with me. Ask me the questions and wait for my answers. Do not write explanations. Ask me the questions one by one like an interviewer does and wait for my answers. My first sentence is Hi`},{role:"user",content: message,  }],
+            messages:[{role:"system",content:`I want you to act as an interviewer. I will be the candidate and you will ask me the interview questions for the position of ReactJS Developer. I want you to only reply as the interviewer.Do not write all the conservation at once. I want you to only do the interview with me. Ask me the questions and wait for my answers. Do not write explanations. Ask me the questions one by one like an interviewer does and wait for my answers.`},{role:"user",content: message,  }],
             max_tokens:100,
         })
     }
